13d577137088e888d5d97642680f5c2660c3e5c4
Daniel Herrero <danielherrero4465@gmail.com>
2024-11-23 13:45:59 +0000

    Main menu made, with ability to change settings, and working play button added

--- HigherOrLower.py
+++ HigherOrLower.py
@@ -1,4 +1,8 @@
 import random, time
+import tkinter as tk
+from tkinter import Tk, Canvas, PhotoImage, font
+from PIL import Image, ImageTk
+
 class Card:
     def __init__(self, number, suit, aceIsEleven, kingsValueMorethan10):
         self.number = number
@@ -59,51 +63,86 @@ class Deck:
 
     def getTopCard(self):
         return self.deck[0]
-    
 
-jokers = True
-aceIsEleven = True
-kingsValueMorethan10 = True
 
-settings = [jokers, aceIsEleven, kingsValueMorethan10]
+def change_cursor(event):
+    global jokersTextOption
+    canvas.config(cursor="hand2")  # Change cursor to a pointing hand
+
+def reset_cursor(event):
+
+    canvas.config(cursor="arrow")  # Reset cursor to default
+
+
 
 def printSettings():
-    print("Here are the current settings:")
+    setitingsStartWidth = 250
 
-    if settings[0]:
-        print("There are jokers in the deck. [1]")
-    else:
-        print("There are no jokers in the deck. [1]")
 
-    if settings[1]:
-        print("The value of the Ace is 11. [2]")
-    else:
-        print("The value of the Ace is 1. [2]")
+    def makeSettingChanges(jokersTextOption, aceTextOption, kingsValueOption):
+        canvas.pack() 
 
-    if settings[2]:
-        print("The Jack, Queen and King are of values 11, 12 and 13 respectively. [3]")
-    else:
-        print("The Jack, Queen and King all are of value 10. [3]")
+        if settings[0]:
+            canvas.itemconfig(jokersTextOption, text="There are jokers in the deck.")
 
-    userinput = input("Enter the number of the setting you would like to change(empty if none): ")
-    if userinput != '':
-        if userinput in ['1','2','3']:
-            settings[int(userinput) - 1] = not settings[int(userinput) - 1]
-            printSettings()
+        else:
+            canvas.itemconfig(jokersTextOption, text="There are no jokers in the deck.")
+
+        if settings[1]:
+            canvas.itemconfig(aceTextOption, text="The value of the Ace is 11.")
+
+        else:
+            canvas.itemconfig(aceTextOption, text="The value of the Ace is 1.")
+
+        if settings[2]:
+            canvas.itemconfig(kingsValueOption, text="The Jack, Queen and King are of values 11, 12 and 13 respectively.")
+
+        else:
+            canvas.itemconfig(kingsValueOption, text="The Jack, Queen and King all are of value 10.")
+
+
+
+    def makeChanges(i, jokersTextOption, aceTextOption, kingsValueOption):
+        global settings
+        print(f"Old values {settings[0]}, {settings[1]}, {settings[2]} )")
+        settings[i] = not settings[i]
+        print(f"New values {settings[0]}, {settings[1]}, {settings[2]} )")
+        makeSettingChanges(jokersTextOption, aceTextOption, kingsValueOption)
+
+    canvas.pack()
+
+    settingsIntro = canvas.create_text(25,200, text="Here are the current settings(click to change):", anchor="w",fill="white",font=font.Font(family="Arial", size=15, underline=True))
 
         
+    jokersTextOption = canvas.create_text(25,setitingsStartWidth, text="There are jokers in the deck.", anchor="w",fill="white",font=("Arial", 15))
+    aceTextOption = canvas.create_text(25,setitingsStartWidth + 50, text="The value of the Ace is 11.", anchor="w",fill="white",font=("Arial", 15))
+    kingsValueOption = canvas.create_text(25,setitingsStartWidth + 100, text="The Jack, Queen and King are of values 11, 12 and 13 respectively.", anchor="w",fill="white",font=("Arial", 15))
+    makeSettingChanges(jokersTextOption, aceTextOption, kingsValueOption)
+
+    canvas.tag_bind(jokersTextOption, "<Enter>", change_cursor)
+    canvas.tag_bind(jokersTextOption, "<Leave>", reset_cursor)
+
+
+    canvas.tag_bind(aceTextOption, "<Enter>", change_cursor)
+    canvas.tag_bind(aceTextOption, "<Leave>", reset_cursor)
+
+
+    canvas.tag_bind(kingsValueOption, "<Enter>", change_cursor)
+    canvas.tag_bind(kingsValueOption, "<Leave>", reset_cursor)
+
+    canvas.tag_bind(jokersTextOption, "<Button-1>", lambda event: makeChanges(0, jokersTextOption, aceTextOption, kingsValueOption))
+
+    canvas.tag_bind(aceTextOption, "<Button-1>", lambda event: makeChanges(1, jokersTextOption, aceTextOption, kingsValueOption))
+
+    canvas.tag_bind(kingsValueOption, "<Button-1>", lambda event: makeChanges(2, jokersTextOption, aceTextOption, kingsValueOption))
 
     
 
 
-        
+ 
     
-def showRules():
-    print("The goal of this game is to guess whether the next card to be shown from the deck will be higher or lower than the current card thats shown. After each guess the card will be revealed.")
-    global rounds
-    while not (rounds > 0 and rounds < 20):
-        rounds = int(input("How many rounds would you like to play? (1-20) ")) 
-    printSettings()
+
+
     
 
 
@@ -143,15 +182,107 @@ def endGame():
 
 
 
+def showRules():
+
+    canvas.pack()
+    introText = canvas.create_text(width/2,25, text="Welcome to Daniel's Higher/Lower Game!", anchor="center",fill="white",font=("Arial", 20))
+    rulestext = canvas.create_text(width/2,100, text="The goal of this game is to guess whether the next card to be shown from the deck will be higher or lower than the current card that's shown. After each guess the card will be revealed.", anchor="center",fill="white",font=("Arial", 15), width=width) 
+    global rounds
+    # while not (rounds > 0 and rounds < 20):
+    #     rounds = int(input("How many rounds would you like to play? (1-20) ")) 
+    rounds = 5
+        
+    printSettings()
 
 
 
+window = Tk() #Creates instance of Tk class
+width = 800
+height = 600
+
+def configureWindow(width, height):
+    geometryInit = str(width)+"x"+str(height)
+    window.configure(background="black")
+    window.title("Higher or Lower")
+    screenWidth = window.winfo_screenwidth()
+    screenHeight = window.winfo_screenheight()
+    xSpace = 0.5*(screenWidth-width)
+    ySpace = 0.5*(screenHeight-height)
+    window.geometry(geometryInit+"+"+str(int(xSpace))+"+"+str(int(ySpace)))
     
+configureWindow(width, height)
+
+
+
+#Creating a canvas
+canvas = Canvas(window, bg="black", width=width, height=height)
+settings = [True, True,True] #First is True if we have jokers, second if the ace is value 11, third if kings value is not 13
+points = 0
 rounds = 0
-deck = Deck(settings[0], settings[1], settings[2])
-print("Welcome to Daniel's Higher/Lower Game!")
-time.sleep(0.5)
+
+
+
+def on_closing():
+    window.destroy()  
+    exit()      
+
+
+
+
+
 showRules()
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+
+
+
+def on_play(event):
+    print("Play button clicked!")
+
+
+image = Image.open("play_button.png") 
+
+max_height = height/4
+
+original_width, original_height = image.size
+new_height = int(max_height)
+new_width = int((max_height / original_height) * original_width)
+
+scaled_image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+play_image = ImageTk.PhotoImage(scaled_image)
+window.play_image = play_image
+
+play_button = canvas.create_image(width / 2, height*13 / 16, image=play_image)
+
+canvas.tag_bind(play_button, "<Button-1>", on_play)
+canvas.tag_bind(play_button, "<Enter>", change_cursor)
+canvas.tag_bind(play_button, "<Leave>", reset_cursor)
+
+canvas.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop() 
+
+deck = Deck(settings[0], settings[1], settings[2])
+
+
+
+
 print("Let's go!")
 time.sleep(1)
 print("3")
@@ -160,7 +291,6 @@ print("2")
 time.sleep(1)
 print("1")
 
-points = 0
 
 deck.shuffle()
 last_value = deck.getTopCard().value
@@ -175,6 +305,8 @@ for i in range(rounds):
     playRound(i+1)
 
 endGame()
+
+
 # showDeck()
             
 
